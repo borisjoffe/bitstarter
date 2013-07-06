0ad9b42b31064cc18053ca0df245980a4c4f877a
Boris Joffe <boris@joff3.com>
2013-07-06 08:55:14 -0700

    added index.html file and made node read from it

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var buf = Buffer(fs.readFileSync('index.html'), 'utf-8');
+
+  response.send(buf.toString('utf-8'))
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
